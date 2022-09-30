da2cc93496bb53d94b404474e514e5c4364833fe
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-09-30 10:00:48 +0200

    Add tabs title with react-helmet

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Helmet} from "react-helmet";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Contact</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      Contact
+    </div>
   )
 }
 
-export default Contact
+export default Contact;
 
