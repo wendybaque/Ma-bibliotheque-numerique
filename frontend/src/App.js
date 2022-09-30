13623d18c70d3b7980adb9766ce2f21824366d4e
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-09-30 09:40:25 +0200

    Add routes with react-router-dom in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
+import Home from "../src/pages/Home";
+import Library from "../src/pages/Library";
+import Signin from "../src/pages/Signin";
+import Signup from "../src/pages/Signup";
+import ForgetPassword from "../src/pages/ForgetPassword";
+import Search from "../src/pages/Search";
+import Favorite from "../src/pages/Favorite";
+import Contact from "../src/pages/Contact";
+import Page404 from "../src/pages/Page404";
+
 
 function App() {
   return (
     <div className="App">
-     MA BIBLIOTHEQUE NUMERIQUE
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgetpassword" element={<ForgetPassword />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
