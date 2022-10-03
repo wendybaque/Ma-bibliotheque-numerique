15fc1909f89906db629413a3235bf1d48809e72b
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-03 15:52:21 +0200

    Add put route and update in frontend

--- backend/index.js
+++ backend/index.js
@@ -33,44 +33,64 @@ app.get("/books/az", (req, res) => {
 
 // GET FAVORITES BOOKS = READ
 app.get("/books/fav", (req, res) => {
-    const query = "SELECT * FROM books WHERE opinion = 5";
-    db.query(query, (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.json(err);
-      }
-      return res.json(data);
-    });
+  const query = "SELECT * FROM books WHERE opinion = 5";
+  db.query(query, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
   });
+});
 
-  // ADD A BOOK = CREATE
-  app.post("/books", (req, res) => {
-    const query = "INSERT INTO books(`title`, `desc`, `author`, `cover`, `opinion`, `publisher`, `genre`) VALUES (?)";
-    const values = [
-      req.body.title,
-      req.body.desc,
-      req.body.author,
-      req.body.cover,
-      req.body.opinion,
-      req.body.publisher,
-      req.body.genre,
-    ];
-    db.query(query, [values], (err, data) => {
-      if (err) return res.send(err);
-      return res.json("Ton livre a bien été ajouté.");
-    });
+// ADD A BOOK = CREATE
+app.post("/books", (req, res) => {
+  const query =
+    "INSERT INTO books(`title`, `desc`, `author`, `cover`, `opinion`, `publisher`, `genre`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.author,
+    req.body.cover,
+    req.body.opinion,
+    req.body.publisher,
+    req.body.genre,
+  ];
+  db.query(query, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json("Ton livre a bien été ajouté");
   });
+});
 
 // DELETE A BOOK = DELETE
 app.delete("/books/:id", (req, res) => {
   const bookId = req.params.id;
-  const query = "DELETE FROM books WHERE id = ?"
+  const query = "DELETE FROM books WHERE id = ?";
   db.query(query, [bookId], (err, data) => {
     if (err) return res.send(err);
     return res.json("Ton livre a bien été supprimé.");
   });
-})
-  
+});
+
+// UPDATE A BOOK = UPDATE
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const query =
+    "UPDATE books SET `title`= ?, `desc`= ?, `author`= ?, `cover`= ?, `opinion`= ?, `publisher`= ?, `genre`= ? WHERE id = ?";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.author,
+    req.body.cover,
+    req.body.opinion,
+    req.body.publisher,
+    req.body.genre,
+  ];
+  db.query(query, [...values, bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json("Ton livre a bien été modifié.");
+  });
+});
 
 // BACKEND CONFIG
 app.get("/", (req, res) => {
