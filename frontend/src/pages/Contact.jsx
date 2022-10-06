8d56081abc0323084d36da4af8fce5c6f242fd98
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-06 16:12:25 +0200

    Add a fade effect in add, update and contact forms

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import emailjs from "emailjs-com";
 import { Helmet } from "react-helmet";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Fade } from "react-awesome-reveal";
 
 function Contact() {
   // Toastify
@@ -13,6 +14,12 @@ function Contact() {
 
   const formRef = useRef();
   const [done, setDone] = useState(false);
+  const [btn, setBtn] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
     emailjs
@@ -33,6 +40,14 @@ function Contact() {
       );
   };
 
+  useEffect(() => {
+    if (name !== "" && email !== "" && subject !== "" && message !== "") {
+      setBtn(true);
+    } else if (btn) {
+      setBtn(false);
+    }
+  }, [name, email, subject, message, btn]);
+
   return (
     <div className="lg:flex lg:flex-row md:flex-col sm:flex-col bg-slate-100 overflow-hidden justify-center">
       <Helmet>
@@ -41,72 +56,92 @@ function Contact() {
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
       <div className="flex flex-col font-bold justify-center items-center text-center ">
-          <h2 className="text-xl text-center font-bold m-2 p-2">Contact</h2>
-          <p> Une question ? Une suggestion ?</p>
-          <p className="text-yellow-600 mb-6">
-            Rendez-vous dans le formulaire ci-dessous.
-          </p>
+        <h2 className="text-xl text-center font-bold m-2 p-2">Contact</h2>
+        <p> Une question ? Une suggestion ?</p>
+        <p className="text-yellow-600 mb-6">
+          Rendez-vous dans le formulaire ci-dessous.
+        </p>
         <form
           ref={formRef}
           onSubmit={handleSubmit}
           className="grid justify-items-center"
         >
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Nom
-            <input
-              type="text"
-              placeholder="Votre nom"
-              name="user_name"
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            E-mail
-            <input
-              type="email"
-              placeholder="Votre e-mail"
-              name="user_email"
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Sujet
-            <input
-              type="text"
-              placeholder="Sujet de votre message"
-              name="user_subject"
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Message
-            <textarea
-              rows="10"
-              placeholder="Votre message"
-              name="message"
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <button
-            type="submit"
-            className="hover:animate-bounce cursor-pointer text-white hover:text-white bg-yellow-600 box-shadow-lg font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-8 ml-8 mt-6"
-            onClick={notify}
-          >
-            Envoyer !
-          </button>
-          {done && <ToastContainer />}
+          <Fade>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Nom
+              <input
+                type="text"
+                placeholder="Votre nom"
+                name="user_name"
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              E-mail
+              <input
+                type="email"
+                placeholder="Votre e-mail"
+                name="user_email"
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Sujet
+              <input
+                type="text"
+                placeholder="Sujet de votre message"
+                name="user_subject"
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+                onChange={(e) => setSubject(e.target.value)}
+                value={subject}
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Message
+              <textarea
+                rows="10"
+                placeholder="Votre message"
+                name="message"
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
+            </label>
+            {done && <ToastContainer />}
+            {btn ? (
+              <button
+                type="submit"
+                className="hover:animate-bounce cursor-pointer text-white hover:text-white bg-yellow-600 box-shadow-lg font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-8 ml-8 mt-6"
+                onClick={notify}
+              >
+                Envoyer
+              </button>
+            ) : (
+              <button
+                disabled
+                type="submit"
+                className="hover:animate-bounce cursor-pointer text-white hover:text-white bg-yellow-200 box-shadow-lg font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-8 ml-8 mt-6"
+              >
+                Envoyer
+              </button>
+            )}
+          </Fade>
         </form>
       </div>
     </div>
--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 
 const Update = () => {
   const [updateBook, setUpdateBook] = useState({
@@ -44,113 +45,115 @@ const Update = () => {
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
       <div className="flex flex-col font-bold justify-center items-center text-center ">
-          <h2 className="text-xl text-center font-bold m-2 p-2">
-            Modifier un livre
-          </h2>
+        <h2 className="text-xl text-center font-bold m-2 p-2">
+          Modifier un livre
+        </h2>
         <form>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Titre du livre
-            <input
-              type="text"
-              placeholder="ex : Les Misérables"
-              name="title"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Auteur du livre
-            <input
-              type="text"
-              placeholder="ex : Victor Hugo"
-              name="author"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Genre du livre
-            <input
-              type="text"
-              placeholder="ex : Roman"
-              name="genre"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Editeur du livre
-            <input
-              type="text"
-              placeholder="ex : Albin Michel"
-              name="publisher"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Résumé du livre
-            <textarea
-              type="text"
-              placeholder="C'est l'histoire de..."
-              name="desc"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Avis sur le livre (note /5)
-            <select
-              name="opinion"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+          <Fade>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Titre du livre
+              <input
+                type="text"
+                placeholder="ex : Les Misérables"
+                name="title"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Auteur du livre
+              <input
+                type="text"
+                placeholder="ex : Victor Hugo"
+                name="author"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Genre du livre
+              <input
+                type="text"
+                placeholder="ex : Roman"
+                name="genre"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Editeur du livre
+              <input
+                type="text"
+                placeholder="ex : Albin Michel"
+                name="publisher"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Résumé du livre
+              <textarea
+                type="text"
+                placeholder="C'est l'histoire de..."
+                name="desc"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Avis sur le livre (note /5)
+              <select
+                name="opinion"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              >
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </label>
+            <label className="grid grid-col mb-2 text-sm font-medium">
+              Couverture du livre (url)
+              <input
+                type="text"
+                placeholder="ex : https://la-couverture-de-mon-livre.jpg"
+                name="cover"
+                onChange={handleChange}
+                required
+                aria-required="true"
+                autoComplete="off"
+                className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
+              />
+            </label>
+            <button
+              type="button"
+              onClick={handleClick}
+              className="hover:animate-bounce cursor-pointer text-white hover:text-white bg-yellow-600 box-shadow-lg font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-8 ml-8 mt-6"
             >
-              <option>0</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </label>
-          <label className="grid grid-col mb-2 text-sm font-medium">
-            Couverture du livre (url)
-            <input
-              type="text"
-              placeholder="ex : https://la-couverture-de-mon-livre.jpg"
-              name="cover"
-              onChange={handleChange}
-              required
-              aria-required="true"
-              autoComplete="off"
-              className="m-2 p-4 w-96 rounded-lg shadow-md cursor-pointer font-normal"
-            />
-          </label>
-          <button
-            type="button"
-            onClick={handleClick}
-            className="hover:animate-bounce cursor-pointer text-white hover:text-white bg-yellow-600 box-shadow-lg font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-8 ml-8 mt-6"
-          >
-            Modifier ce livre
-          </button>
+              Modifier ce livre
+            </button>
+          </Fade>
         </form>
         {error && "Something went wrong!"}
       </div>
